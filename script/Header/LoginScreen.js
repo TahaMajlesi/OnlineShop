f7c283e3ae7ebd77e6a3ec26fc3ae945519db5ee
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2024-11-05 22:58:07 -0800

    responsive fixed

--- script/Header/LoginScreen.js
+++ script/Header/LoginScreen.js
@@ -8,6 +8,15 @@ $(document).ready(function() {
             $("#loginScreen").removeClass("flex");
         }
     })
+    $("#mobileUserBtn").on("click", function() {
+        if ($("#loginScreen").hasClass("hidden")) {
+            $("#loginScreen").removeClass("hidden");
+            $("#loginScreen").addClass("flex");
+        } else {
+            $("#loginScreen").addClass("hidden");
+            $("#loginScreen").removeClass("flex");
+        }
+    })
     $("#closelogin").on("click", function() {
         if ($("#loginScreen").hasClass("flex")) {
             $("#loginScreen").addClass("hidden");
